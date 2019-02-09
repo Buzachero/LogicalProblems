30f731d5b552d391075e3eea2fcbc9254cde2c48
Buzachero <henrique_buzachero@hotmail.com>
2019-02-09 11:52:31 -0200

    Enhancing EletronicsShop resolution

--- src/main/java/br/com/buzachero/EletronicsShop.java
+++ src/main/java/br/com/buzachero/EletronicsShop.java
@@ -1,8 +1,6 @@
 package br.com.buzachero;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class EletronicsShop {
 
@@ -11,27 +9,22 @@ public class EletronicsShop {
 		int tempSum = 0;			
 		Arrays.sort(keyboards);
 		Arrays.sort(drives);		
-		List<Integer> maxElements = new ArrayList<Integer>();
 		
 		for(int i = keyboards.length-1; i >= 0; i--) {
 			for (int j = drives.length-1; j >= 0; j--) {
 				tempSum = keyboards[i]+drives[j];
 				if(tempSum <= b) {
-					maxElements.add(tempSum);
+					if(tempSum >= maxSum) {
+						maxSum = tempSum;
+					}
 					break;
 				}
 			}
 		}
 		
-		if(maxElements.size() == 0) {
+		if(maxSum == 0) {
 			return -1;
 		}	
-		
-		for (Integer integer : maxElements) {
-			if(integer >= maxSum) {
-				maxSum = integer;
-			}
-		}
 				
 		return maxSum;
 	}
